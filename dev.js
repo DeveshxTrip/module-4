76aec2dfe803e4a9a16310f5ff1b7927de6b44e6
Devesh Tripathi <93699872+DeveshxTrip@users.noreply.github.com>
2022-01-02 22:37:07 +0530

    Add files via upload

---
+++ dev.js
@@ -0,0 +1,13 @@
+function sayhello() {
+   var name = document.getElementById("name").value;
+   var msg="<h1>Hello " + name  + " !</h1>";
+var name1 =name;
+   document
+   .getElementById("content")
+   //.textContent=msg;
+   .innerHTML=msg;
+  }
+  if(name1 ==="harshit"){
+     console.log("Madarchod");
+  }
+  
